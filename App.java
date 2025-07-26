a3ed76a6becc9d563e502c4b7bdeef0bf1477cbb
unknown <prabhnoorsingh85@gmail.com>
2025-07-26 23:51:31 +0200

    difference between declaring string normally and using the new method

--- App.java
+++ App.java
@@ -184,3 +184,27 @@
 //         System.out.println(--number);
 //     }
 // }
+
+// class App{
+//     public static void main(String[]args){
+//         //String name = "Prabhnoor Singh";
+
+//         String name = new String("Prabhnoor SIngh");
+
+//         System.out.println(name);
+//     }
+// }
+
+class App{
+    public static void main(String[]args){
+        String literalString1 = "abc";
+        String literalString2 = "abc";
+
+        String ObjectString1 = new String("xyz");
+        String ObjectString2 = new String("xyz");
+
+        System.out.println(literalString1 == literalString2);
+        System.out.println(ObjectString1 == ObjectString2);
+        
+    }
+}
