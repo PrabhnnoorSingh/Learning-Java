9587041e008282c40e19ea9075274689d32c1057
unknown <prabhnoorsingh85@gmail.com>
2025-07-27 12:53:13 +0200

    difference between normal method to declare a string and the new method.

--- App.java
+++ App.java
@@ -195,16 +195,16 @@
 //     }
 // }
 
-class App{
-    public static void main(String[]args){
-        String literalString1 = "abc";
-        String literalString2 = "abc";
+// class App{
+//     public static void main(String[]args){
+//         String literalString1 = "abc";
+//         String literalString2 = "abc";
 
-        String ObjectString1 = new String("xyz");
-        String ObjectString2 = new String("xyz");
+//         String ObjectString1 = new String("xyz");
+//         String ObjectString2 = new String("xyz");
 
-        System.out.println(literalString1 == literalString2);
-        System.out.println(ObjectString1 == ObjectString2);
+//         System.out.println(literalString1 == literalString2);
+//         System.out.println(ObjectString1 == ObjectString2);
         
-    }
-}
+//     }
+// }
