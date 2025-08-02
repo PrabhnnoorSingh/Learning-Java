d904dcf47bf5b5605a47ef7d2ccb5f9924e002f2
unknown <prabhnoorsingh85@gmail.com>
2025-08-02 17:14:16 +0200

    Replacing a value in an array

--- App.java
+++ App.java
@@ -607,3 +607,14 @@
 //     }
 // }
 
+                        //Replacing a value in an Array
+// import java.util.Arrays;
+// class App{
+//     public static void main(String[]args){
+//         char vowels[] = {'a','e','i','o','u'};
+
+//         vowels[2] = 'x';
+
+//         System.out.println(Arrays.toString(vowels));
+//     }
+// }
