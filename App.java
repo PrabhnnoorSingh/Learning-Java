9d405724a835717c8bfe7ad4ec836fc09855a7d6
unknown <prabhnoorsingh85@gmail.com>
2025-08-02 16:46:06 +0200

    using Arrays

--- App.java
+++ App.java
@@ -579,3 +579,20 @@
 //         scanner.close();
 //     }
 // }end
+
+//                         //Using Arrays
+// import java.util.Arrays;
+// class App{
+//     public static void main(String[]args){
+//         char vowels[] = new char [5];
+        
+//         vowels[0] ='a';
+//         vowels[1] ='e';
+//         vowels[2] ='i';
+//         vowels[3] ='o';
+//         vowels[4] ='u';
+
+
+//         System.out.println(Arrays.toString(vowels));
+//     }
+// }
