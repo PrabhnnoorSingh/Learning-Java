daf2251efc42ada7566c86e7dc1f52d082ed4b52
unknown <prabhnoorsingh85@gmail.com>
2025-08-11 15:56:46 +0200

    Adding an array using a FOR loop

--- App.java
+++ App.java
@@ -791,3 +791,16 @@
 //         }
 //     }
 // }//
+
+//                         //Adding an array using a FOR loop
+// class App{
+//     public static void main(String[]args){
+//         int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//         int sum = 0;
+//         for(int index = 0; index < 10; index++){
+//             sum += numbers[index];
+          
+//         }
+//           System.out.println(sum);
+//     }
+// }
