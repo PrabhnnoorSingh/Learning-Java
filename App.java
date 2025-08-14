3cac6ff1f6744f1cc82093b88a3b7a1377956c8b
unknown <prabhnoorsingh85@gmail.com>
2025-08-14 16:38:36 +0200

    Using do while loops

--- App.java
+++ App.java
@@ -893,3 +893,16 @@
 //         }
 //     }
 // }--
+
+//                         //do while loops
+// class App{
+//     public static void main(String[]args){
+//         int number = 5;
+//         int multiplier = 1;
+
+//         do{
+//             System.out.printf("%d X %d = %d\n",number,multiplier,number*multiplier);
+//             multiplier++;
+//         }while(multiplier <=10);
+//     }
+// }
