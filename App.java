576c853c544e7a4a61c095107af4be10329937b9
unknown <prabhnoorsingh85@gmail.com>
2025-08-15 17:06:00 +0200

    Printing certain index in an ArrayList

--- App.java
+++ App.java
@@ -922,3 +922,19 @@
 //         System.out.println(numbers.toString());
 //     }
 // }--
+
+//                         //Printing certain index in an ArrayList
+// import java.util.ArrayList;
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(1); //0---
+//         numbers.add(2); //1  |
+//         numbers.add(3); //2  Index
+//         numbers.add(4); //3  |
+//         numbers.add(5); //4---
+
+//         System.out.println(numbers.get(2));
+//     }
+// }
