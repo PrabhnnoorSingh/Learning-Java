b3543a7764b71a84f576157df1400105a8b6a62e
unknown <prabhnoorsingh85@gmail.com>
2025-08-15 17:02:06 +0200

    Using ArrayList

--- App.java
+++ App.java
@@ -906,3 +906,19 @@
 //         }while(multiplier <=10);
 //     }
 // }
+
+//                         //Array list
+// import java.util.ArrayList;
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(1);
+//         numbers.add(2);
+//         numbers.add(3);
+//         numbers.add(4);
+//         numbers.add(5);
+
+//         System.out.println(numbers.toString());
+//     }
+// }--
