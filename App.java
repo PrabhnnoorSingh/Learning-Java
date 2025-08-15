bb050e302061e8acdd5eb18d384f93b5e4094da6
unknown <prabhnoorsingh85@gmail.com>
2025-08-15 17:31:36 +0200

    Emptying an ArrayList

--- App.java
+++ App.java
@@ -974,3 +974,21 @@
 //         System.out.println(numbers.toString());
 //     }
 // }
+
+//                         //Emptying an ArrayList
+// import java.util.ArrayList;
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(1);
+//         numbers.add(2);
+//         numbers.add(3);
+//         numbers.add(4);
+//         numbers.add(5);
+
+//         numbers.clear();
+
+//         System.out.println(numbers.toString());
+//     }
+// }--
