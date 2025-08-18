eafe178f8f671bfdc3dbbad78e48ed695073ec8a
unknown <prabhnoorsingh85@gmail.com>
2025-08-18 18:27:00 +0200

    Sorting an ArrayList

--- App.java
+++ App.java
@@ -1010,3 +1010,23 @@
 //         System.out.println(numbers.toString());
 //     }
 // }
+
+//                         //Sorting an ArrayList
+// import java.util.ArrayList;
+// import java.util.Comparator;
+
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(5);
+//         numbers.add(3);
+//         numbers.add(4);
+//         numbers.add(1);
+//         numbers.add(2);
+
+//         numbers.sort(Comparator.naturalOrder());
+
+//         System.out.println(numbers.toString());
+//     }
+// }--
