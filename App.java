f7c3a190fdc1511abad790514a5de829e9f4544e
unknown <prabhnoorsingh85@gmail.com>
2025-08-18 18:30:04 +0200

    sorting an ArrayList in the reverse order

--- App.java
+++ App.java
@@ -1030,3 +1030,23 @@
 //         System.out.println(numbers.toString());
 //     }
 // }--
+
+//                         //Sorting an ArrayListi in the reverse order
+// import java.util.ArrayList;
+// import java.util.Comparator;
+
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(5);
+//         numbers.add(3);
+//         numbers.add(4);
+//         numbers.add(1);
+//         numbers.add(2);
+
+//         numbers.sort(Comparator.reverseOrder());
+
+//         System.out.println(numbers.toString());
+//     }
+// }
