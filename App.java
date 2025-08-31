d4f10f32742bf74bc8f3414bbf1ec522b8f71291
unknown <prabhnoorsingh85@gmail.com>
2025-08-31 15:04:30 +0200

    using the .size method

--- App.java
+++ App.java
@@ -1066,3 +1066,26 @@
 
 //     }
 // }--
+
+//                         //Using the .size method 
+// import java.util.ArrayList;
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(1);
+//         numbers.add(2);
+//         numbers.add(3);
+//         numbers.add(4);
+//         numbers.add(5);
+//         numbers.add(6);
+//         numbers.add(7);
+//         numbers.add(8);
+//         numbers.add(9);
+//         numbers.add(0);
+
+//         System.out.println(numbers.size());
+
+//     }
+
+// }
