52cb9d4b1b35f2d5af5860c27c55f43b9a44bc4d
unknown <prabhnoorsingh85@gmail.com>
2025-09-13 17:37:15 +0200

    Clearing an ArrayList

--- App.java
+++ App.java
@@ -1111,3 +1111,27 @@
 
 //     }
 // }--
+
+//                         //Clearing an ArryaList
+// import java.util.ArrayList;
+// class App{
+//     public static void main(String[]args){
+//         ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+//         numbers.add(1);
+//         numbers.add(2);
+//         numbers.add(3);
+//         numbers.add(4);
+//         numbers.add(5);
+//         numbers.add(6);
+//         numbers.add(7);
+//         numbers.add(8);
+//         numbers.add(9);
+//         numbers.add(0);
+
+//         numbers.clear();
+
+//         System.out.println(numbers.isEmpty());
+
+//     }
+// }
